98d8eadde7343aff443381bf8cdfe9ba2631e105
alexdydalin <alexdydalin3@gmail.com>
2023-04-15 22:00:37 +0500

    question_set

--- src/modules/questions/view/questions_view.py
+++ src/modules/questions/view/questions_view.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from flask_pydantic import validate
+from peewee import fn
 
 from src.modules.auth.model.user_token_data import UserTokenData
 from src.modules.questions.model.get_questions_request import GetQuestionsRequest
@@ -17,3 +18,9 @@ def get_questions(data: UserTokenData, body: GetQuestionsRequest):
     return questions
 
 
+@questions_view.route('/get_question_set', methods=["GET"])
+@validate()
+@tokenized
+def get_question_set(data: UserTokenData, body: GetQuestionsRequest):
+    question_set = QuestionModel.select().where(QuestionModel.technology == body.technology).order_by(fn.Rand()).limit(15)
+    return question_set
