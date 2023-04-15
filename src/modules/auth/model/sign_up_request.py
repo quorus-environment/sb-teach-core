f3b06af81806b0b6d2f10a85e20312b66f35043c
Konstantine <Konstaphy@gmail.com>
2023-04-15 13:52:17 +0500

    соединил модели реквеста с вьюшкой

--- src/modules/auth/model/sign_up_request.py
+++ src/modules/auth/model/sign_up_request.py
@@ -2,4 +2,9 @@ from pydantic import BaseModel
 
 class SignUpRequest(BaseModel):
     username: str
+    email: str
     password: str
+
+    first_name: str
+    last_name: str
+    second_name: str
--- src/modules/auth/view/auth_view.py
+++ src/modules/auth/view/auth_view.py
@@ -1,13 +1,14 @@
 from flask import Blueprint, jsonify
 from flask_pydantic import validate
 
+from src.modules.auth.model.sign_in_request import SignInRequest
 from src.modules.auth.model.sign_up_request import SignUpRequest
 
 auth_view = Blueprint('auth', __name__, url_prefix="/auth")
 
 # Регистрация
 @auth_view.route('/sign-in', methods=["POST"])
-def sign_in():
+def sign_in(body: SignInRequest):
     # Тут нужно создать нового пользователя с определенной ролью и
     # сгенерить токен и отправить его на фронт
     return jsonify({"token": "token"})
