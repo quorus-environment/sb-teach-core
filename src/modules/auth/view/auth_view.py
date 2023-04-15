76ba7cd0ff008272651b16a4921b1174e902e8eb
alexdydalin <alexdydalin3@gmail.com>
2023-04-15 13:20:57 +0500

    добавил модель юзера

--- src/modules/auth/view/auth_view.py
+++ src/modules/auth/view/auth_view.py
@@ -5,9 +5,10 @@ auth_view = Blueprint('auth', __name__, url_prefix="/auth")
 # Регистрация
 @auth_view.route('/sign-in', methods=["POST"])
 def sign_in():
+
     # Тут нужно создать нового пользователя с определенной ролью и
     # сгенерить токен и отправить его на фронт
-    return jsonify({"token": "token"})
+    return jsonify({"token": token})
 
 @auth_view.route('/sign-up', methods=["POST"])
 def sign_up():
