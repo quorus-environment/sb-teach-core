77426f10a18e82a07bd1835e1d77132d9be0cb7e
Konstantine <Konstaphy@gmail.com>
2023-04-16 07:02:35 +0500

    менторы

--- src/modules/applicants/view/applicants_view.py
+++ src/modules/applicants/view/applicants_view.py
@@ -11,3 +11,12 @@ applicants_view = Blueprint('applicants', __name__, url_prefix="/applicants")
 def get_applicants():
     applicants = User.select().where(User.is_tested).dicts()
     return jsonify({"applicants": [app for app in applicants]})
+
+
+@applicants_view.route('/find-mentors', methods=["GET"])
+@cross_origin(supports_credentials=True)
+def get_mentors():
+    mentors = User.select().dicts()
+    mentors = [mentor for mentor in filter(lambda x: "mentor" in x.roles, mentors)]
+    return jsonify({"mentors": [mentor for mentor in mentors]})
+
